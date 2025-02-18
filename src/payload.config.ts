6df767740527da9bf88590f02304a537bc9d9fe3
Arnold Truong <vinhhao.truong.52@gmail.com>
2025-02-18 22:31:24 +1100

    First setup

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,11 +1,14 @@
 // storage-adapter-import-placeholder
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
+import { vi } from '@payloadcms/translations/languages/vi'
+import { en } from '@payloadcms/translations/languages/en'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { s3Storage } from '@payloadcms/storage-s3'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -14,6 +17,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  i18n: { supportedLanguages: { vi, en } },
   admin: {
     user: Users.slug,
     importMap: {
@@ -28,12 +32,32 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URI || '',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT || '5432'),
+      user: process.env.POSTGRES_USER || '',
+      password: process.env.POSTGRES_PASSWORD || '',
+      database: process.env.POSTGRES_DATABASE || 'postgres',
     },
   }),
   sharp,
   plugins: [
     payloadCloudPlugin(),
     // storage-adapter-placeholder
+    s3Storage({
+      collections: {
+        media: true,
+      },
+      bucket: process.env.S3_BUCKET || 'bucket',
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID || 'supersecret',
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || 'supersecret',
+        },
+        region: process.env.S3_REGION,
+        // ... Other S3 configuration
+        endpoint: process.env.S3_ENDPOINT || 'supersecret',
+        forcePathStyle: true,
+      },
+    }),
   ],
 })
