e7a67c0362bf41885b83768e5a3b1251679b7571
Arnold Truong <vinhhao.truong.52@gmail.com>
2025-05-12 23:10:17 +1000

    fix eslint build err

--- src/libs/utils/get/getHexFromString.ts
+++ src/libs/utils/get/getHexFromString.ts
@@ -1,4 +1,4 @@
-export default function (str: string) {
+export default function getHexFromString(str: string) {
   let hash = 0;
 
   // Generate hash from string
@@ -7,7 +7,7 @@ export default function (str: string) {
   }
 
   // Convert hash to 6-digit hex
-  let hex = (hash & 0x00ffffff).toString(16).toUpperCase();
+  const hex = (hash & 0x00ffffff).toString(16).toUpperCase();
 
   // Pad with zeros if needed
   return "#" + "00000".substring(0, 6 - hex.length) + hex;
